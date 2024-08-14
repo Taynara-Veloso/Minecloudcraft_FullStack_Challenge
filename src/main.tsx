688c3daaec04cd6908ae881e3c36f2119c56939d
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-13 23:27:53 -0300

    :star: main refactoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import './styles/global.css'
+import React from 'react'
 import App from './App.tsx'
-import './global.css'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
