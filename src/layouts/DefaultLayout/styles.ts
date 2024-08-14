bdf6a8d5eff0bda5ebb76eb8d302ead99506c4dd
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-13 23:27:14 -0300

    :star: DefaultLayout - css

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 70rem;
+  height: calc(100vh - 5rem);
+  margin: 2.5rem auto;
+  padding-top: 2rem;
+
+  background: var(--gray-700);
+  border-radius: 8px;
+
+  display: flex;
+  flex-direction: column;
+`
