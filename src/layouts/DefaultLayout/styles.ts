2de6cc1e598023199d39b2ac3d7a1cf6437c51a5
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-14 16:19:30 -0300

    :star: DefaultLayout : border and box-shadow

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -5,6 +5,8 @@ export const LayoutContainer = styled.div`
   height: calc(100vh - 5rem);
   margin: 2.5rem auto;
   padding-top: 2rem;
+  border: 1px solid var(--green-600);
+  box-shadow: 1px 2px 11px 0px black;
 
   background: var(--gray-700);
   border-radius: 8px;
