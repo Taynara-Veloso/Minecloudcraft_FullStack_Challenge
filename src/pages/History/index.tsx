11f752213c7d8662c41ed9e00f178e2e9b66d6f6
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-15 21:29:41 -0300

    :star: Page History: table structure

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,44 @@
+export function History(){
+  return (
+    <div className="flex flex-1 flex-col w-[900px] m-auto my-12">
+      <h1 className="text-2xl font-semibold mx-3 my-4 drop-shadow-[6px_6px_6px_rgba(0,0,0,50)]">History</h1>
+
+      <div className="relative overflow-x-auto rounded-t-xl">
+        <table className="w-full text-sm text-left rtl:text-right">
+          <thead className="text-lg bg-[--gray-500]">
+            <tr className="border-b-8 border-[--gray-700]">
+              <th scope="col" className="px-6 py-3 text-center border-r-4 border-[--gray-700]">
+                Host
+              </th>
+              <th scope="col" className="px-6 py-3 text-center border-r-4 border-[--gray-700]">
+                Players
+              </th>
+              <th scope="col" className="px-6 py-3 text-center border-r-4 border-[--gray-700]">
+                Map
+              </th>
+              <th scope="col" className="px-6 py-3 text-center">
+                Mode
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-[--gray-800] border-b-4 border-[--gray-700] text-base">
+              <th scope="row" className="px-6 py-4 font-light whitespace-nowrap text-white truncate text-center border-r-4 border-[--gray-700]">
+                Taynara Santos
+              </th>
+              <td className="px-6 py-4 text-center border-r-4 border-[--gray-700]">
+                4 Players
+              </td>
+              <td className="px-6 py-4 text-center border-r-4 border-[--gray-700]">
+                Random
+              </td>
+              <td className="px-6 py-4 text-center">
+                Creative
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
