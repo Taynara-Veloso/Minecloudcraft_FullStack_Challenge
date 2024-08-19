2070768711109adace3bab24b655c80defd51bf9
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-18 21:46:25 -0300

    :star: history: add session.map((listSessions))

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,9 @@
+import { useContext } from "react"
+import { GameSessionContext } from "../../contexts/SessionContext"
+
 export function History(){
+  const { session } = useContext(GameSessionContext)
+
   return (
     <div className="flex flex-1 flex-col w-[900px] m-auto my-12">
       <h1 className="text-2xl font-semibold mx-3 my-4 drop-shadow-[6px_6px_6px_rgba(0,0,0,50)]">History</h1>
@@ -22,20 +27,24 @@ export function History(){
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-[--gray-800] border-b-4 border-[--gray-700] text-base">
-              <th scope="row" className="px-6 py-4 font-light whitespace-nowrap text-white truncate text-center border-r-4 border-[--gray-700]">
-                Taynara Santos
-              </th>
-              <td className="px-6 py-4 text-center border-r-4 border-[--gray-700]">
-                4 Players
-              </td>
-              <td className="px-6 py-4 text-center border-r-4 border-[--gray-700]">
-                Random
-              </td>
-              <td className="px-6 py-4 text-center">
-                Creative
-              </td>
-            </tr>
+            {session.map((listSessions) => {
+              return (
+                <tr key={listSessions.sessionId} className="bg-[--gray-800] border-b-4 border-[--gray-700] text-base">
+                  <th scope="row" className="px-6 py-4 font-light whitespace-nowrap text-white truncate text-center border-r-4 border-[--gray-700]">
+                    {listSessions.hostname}
+                  </th>
+                  <td className="px-6 py-4 text-center border-r-4 border-[--gray-700]">
+                    {listSessions.players}
+                  </td>
+                  <td className="px-6 py-4 text-center border-r-4 border-[--gray-700]">
+                    {listSessions.gamemap}
+                  </td>
+                  <td className="px-6 py-4 text-center">
+                    {listSessions.gamemode}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </div>
