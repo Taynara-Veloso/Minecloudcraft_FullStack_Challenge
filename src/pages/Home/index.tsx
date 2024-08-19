44b21f47db77f81d80160dcb52b9a69db085a9f8
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-18 21:44:36 -0300

    :star: home: separating the form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,51 @@
-import { CaretDown } from "phosphor-react";
+import * as zod from 'zod'
+import { useContext } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormProvider, useForm } from "react-hook-form";
+import { GameSessionContext } from '../../contexts/SessionContext';
+import { NewSessionForm } from './components/NewSessionForm';
+
+const newFormGameSessionValidationSchema = zod.object({
+  hostname: zod.string().min(1, 'enter hostname'),
+  players: zod
+    .number()
+    .min(1, 'The session must have at least 1 player')
+    .max(4, 'The session must have a maximum of 4 players'),
+  gamemap: zod.string().max(1, 'Select an option'),
+  gamemode: zod.string().max(1, 'Select an option'),
+})
+
+type CreateSessionData = zod.infer<typeof newFormGameSessionValidationSchema>
 
 export function Home(){
+  const { createNewSession } = useContext(GameSessionContext)
+
+  const newGameSessionForm = useForm<CreateSessionData>({
+    resolver: zodResolver(newFormGameSessionValidationSchema),
+    defaultValues: {
+      hostname: '',
+      players: 0,
+      gamemap: 'Choose the map',
+      gamemode: 'Choose mode',
+    },
+  })
+
+  const { handleSubmit, watch, reset } = newGameSessionForm
+
+  const hostnameWatch = watch('hostname')
+  const isSubmitDisabled = !hostnameWatch
+
+  function handleCreateNewGameSession(data: CreateSessionData){
+    createNewSession(data)
+    reset()
+  }
+
   return (
-    <form id="submit_form" className="flex flex-col justify-center m-auto">
-      <fieldset className="flex justify-center py-4 w-[680px]">
-        <div className="flex flex-col justify-center w-full text-[--gray-100] space-y-3">
-          <div className="flex flex-col px-10">
-            <label className="flex justify-center text-2xl font-semibold pb-2">Host</label>
-            <input className="h-12 rounded-xl bg-[--gray-900] text-center shadow-shape" type="text" id="event-host" aria-label="" placeholder="Enter the creator's name" required />
-          </div>
-
-          <div className="flex flex-col px-10">
-            <label className="flex justify-center text-2xl font-semibold pb-2">Players</label>
-            <input className="h-12 rounded-xl bg-[--gray-900] text-center shadow-shape" type="text" id="event-players" aria-label="" placeholder="Add players" required />
-          </div>
-
-          <div className="flex flex-col px-10 relative">
-            <label className="flex justify-center text-2xl font-semibold pb-2">Map</label>
-            <CaretDown className="size-5 pointer-events-none absolute top-3/4 transform -translate-y-1/2 right-14"/>
-            <input className="h-12 rounded-xl bg-[--gray-900] text-center shadow-shape" type="text" id="event-map" aria-label="" placeholder="Choose the map" required />
-          </div>
-
-          <div className="flex flex-col px-10 relative">
-            <label className="flex justify-center text-2xl font-semibold pb-2">Mode</label>
-            <CaretDown className="size-5 pointer-events-none absolute top-3/4 transform -translate-y-1/2 right-14"/>
-            <input className="h-12 rounded-xl bg-[--gray-900] text-center shadow-shape" type="text" id="event-mode" aria-label="" placeholder="Choose mode" required />
-          </div>
-        </div>
-      </fieldset>
-      <button className="w-[610px] min-w-96 m-auto bg-[--green-600] text-white rounded-2xl py-3 shadow-shape">
+    <form className="flex flex-col justify-center m-auto">
+      <FormProvider {...newGameSessionForm}>
+        <NewSessionForm/>
+      </FormProvider>
+      <button onSubmit={handleSubmit(handleCreateNewGameSession)} disabled={isSubmitDisabled} className="w-[610px] min-w-96 m-auto bg-[--green-600] text-white rounded-2xl py-3 shadow-shape">
         Create new game session
       </button>
     </form>
