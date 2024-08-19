f7b402305c99a4016a590cc2ee37a03d948a5dfa
taynara veloso <taynarasantos2.3@hotmail.com>
2024-08-19 18:44:05 -0300

    :star:SessionContextProvider: API_URL

--- src/contexts/SessionContext.tsx
+++ src/contexts/SessionContext.tsx
@@ -1,6 +1,12 @@
-import { createContext, ReactNode, useState } from "react"
+import axios from "axios";
+import { createContext, 
+         ReactNode, 
+         useEffect, 
+         useState 
+} from "react"
 
 interface CreateSessionData {
+  id: string;
   hostname: string;
   players: number;
   gamemap: string;
@@ -9,9 +15,13 @@ interface CreateSessionData {
 
 interface SessionContextType {
   sessions: CreateSessionData[];
+  currentSession: CreateSessionData | undefined;
+  gameSessionId: string | null;
+  API_URL: string;
   createNewSession: (data: CreateSessionData) => void;
 }
 
+const API_URL = 'https://1jz615yn7a.execute-api.us-east-1.amazonaws.com';
 export const GameSessionContext = createContext({} as SessionContextType)
 
 interface SessionContextProviderProps {
@@ -20,14 +30,41 @@ interface SessionContextProviderProps {
 
 export const SessionContextProvider: React.FC<SessionContextProviderProps> = ({ children }) => {
   const [sessions, setSessions] = useState<CreateSessionData[]>([]);
+  const [gameSessionId, setGameSessionsId] = useState<string | null>(null);
 
-  function createNewSession(data: CreateSessionData){
-    console.log('sessionData:', data)
-    setSessions(prevSessions => [...prevSessions, data])
+  useEffect(() => {
+    const fetchSessions = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/sessions`);
+        setSessions(response.data);
+      } catch (error) {
+        console.error("Error fetching sessions", error);
+      }
+    };
+
+    fetchSessions();
+  }, []);
+
+  
+  const createNewSession = async (data: CreateSessionData) => {
+    try {
+      await axios.post(`${API_URL}/sessions`, data);
+
+      const response = await axios.get(`${API_URL}/sessions`);
+      setSessions(response.data);
+    } catch (error) {
+      console.error('Error creating session', error);
+    }
+    setGameSessionsId(data.id);
+
   }
 
+  const currentSession = sessions.find((gameSession) => gameSession.id === gameSessionId)
+
+  console.log(currentSession)
+
   return (
-    <GameSessionContext.Provider value={{createNewSession, sessions}}>
+    <GameSessionContext.Provider value={{createNewSession, sessions, currentSession, gameSessionId, API_URL}}>
       {children}
     </GameSessionContext.Provider>
   )
